a9e6f07315aa11c4f77cec56a0eaa844aa0b0ef1
Ashja <ashja@live.co.uk>
2017-02-24 15:26:20 +0000

    replacing python arrays with numpy arrays

--- boids/boids.py
+++ boids/boids.py
@@ -29,11 +29,33 @@ class Boids(object):
         
 
     def fly_towards_middle(self, strength):
-        
+        # Fly towards the middle
+        middle=np.mean(self.positions, 1)
+        direction_to_middle = self.positions - middle[:, np.newaxis]
+        self.velocities -= direction_to_middle * strength
 
     def avoid_nearby_boids(self, alert_distance):
-        
+        # Fly away from nearby boids
+        separations = self.positions[:,np.newaxis,:] - self.positions[:,:,np.newaxis]
+        squared_displacements = separations * separations
+        square_distances = np.sum(squared_displacements, 0)
+        far_away = square_distances > alert_distance
+        separations_if_close = np.copy(separations)
+        separations_if_close[0,:,:][far_away] =0
+        separations_if_close[1,:,:][far_away] =0
+        self.velocities += np.sum(separations_if_close,1) 
+
     def match_speeds(self, formation_flying_distance, formation_flying_strength):
+   	    # Try to match speed with nearby boids
+        separations = self.positions[:,np.newaxis,:] - self.positions[:,:,np.newaxis]
+        squared_displacements = separations * separations
+        square_distances = np.sum(squared_displacements, 0)
+        velocity_differences = self.velocities[:,np.newaxis,:] - self.velocities[:,:,np.newaxis]
+        very_far=square_distances > formation_flying_distance
+        velocity_differences_if_close = np.copy(velocity_differences)
+        velocity_differences_if_close[0,:,:][very_far] =0
+        velocity_differences_if_close[1,:,:][very_far] =0
+        return (-1) * np.mean(velocity_differences_if_close, 1) * formation_flying_strength
    	    
        
 
