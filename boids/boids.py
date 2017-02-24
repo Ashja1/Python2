8d365e6b47c493dac9fcf67c5805a32fb8dcb6d4
Ashja <ashja@live.co.uk>
2017-02-24 15:31:35 +0000

    replacing constants with config file by combining them into a dictionary which can be loaded from a YAML file

--- boids/boids.py
+++ boids/boids.py
@@ -18,15 +18,17 @@ class Boids(object):
 		self.position_lower_limit=np.array([self.config['Boids']['xlim'][0], self.config['Boids']['ylim'][0]])
 		self.position_upper_limit=np.array([self.config['Boids']['xlim'][1], self.config['Boids']['ylim'][1]])
 		self.velocity_lower_limit=np.array([self.config['Boids']['vxlim'][0], self.config['Boids']['vylim'][0]])
-		self.velocity_upper_limit=np.array([self.config['Boids']['vxlim'][1], self.config['Boids']['vylim'][1]]) 
-		
+		self.velocity_upper_limit=np.array([self.config['Boids']['vxlim'][1], self.config['Boids']['vylim'][1]])
+
+		# generate random boid positions and velocities within given limits
+		self.positions = self.new_flock(self.boid_count, self.position_lower_limit, self.position_upper_limit)
+		self.velocities = self.new_flock(self.boid_count, self.velocity_lower_limit, self.velocity_upper_limit)
 		
 
     def new_flock(self, count, lower_limits, upper_limits):
         width=upper_limits-lower_limits
         return (lower_limits[:,np.newaxis] + 
             np.random.rand(2, count)*width[:,np.newaxis])
-        
 
     def fly_towards_middle(self, strength):
         # Fly towards the middle
@@ -56,16 +58,28 @@ class Boids(object):
         velocity_differences_if_close[0,:,:][very_far] =0
         velocity_differences_if_close[1,:,:][very_far] =0
         return (-1) * np.mean(velocity_differences_if_close, 1) * formation_flying_strength
-   	    
        
 
     def update_boids(self):
-        
+        # apply boids model
+        self.fly_towards_middle(self.fly_to_middle)  
+        self.avoid_nearby_boids(self.alert_distance) 
+        self.velocities += self.match_speeds(self.formation_flying_distance, self.formation_flying_strength)
+        #update positions acc to models
+        self.positions += self.velocities
 
     def animate(self,frame, scatter):
-        
+        # animation runs update_boids() for each frame
+        self.update_boids()
+        scatter.set_offsets(zip(self.positions[0], self.positions[1]))
 
     def simulate(self):
-        
+        #open plot for simulation and run animation
+        figure=plt.figure()
+        axes=plt.axes(xlim=self.config['Animation']['xlim'], ylim=self.config['Animation']['ylim'])
+        scatter=axes.scatter(self.positions[0],self.positions[1])
+        anim = animation.FuncAnimation(figure, self.animate, fargs=[scatter], 
+                                       frames=self.config['Animation']['frames'], interval=self.config['Animation']['interval'])
+        plt.show()
 
 
